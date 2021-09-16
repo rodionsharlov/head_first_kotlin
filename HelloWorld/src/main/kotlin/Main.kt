3d74839b96c5cefc6bd2128655963918f78bcba6
unknown <rodionsharlov@gmail.com>
2021-09-16 15:41:38 +0800

    Done with Chapter 1

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
 fun main(args: Array<String>) {
-    println("Pow!")
+    //
+    var x = 1
+    println("Before the loop. x = $x.")
+    while (x < 4) {
+        println("In the loop. x = $x.")
+        x = x + 1
+    }
+    println("After the loop. x = $x.")
+    println("---")
+
+    //
+    x = 3
+    val y = 1
+    if (x > y )
+        println("x is greater than y")
+    else
+        println("x is not greater than y")
+    println("This line runs no matter what")
+    println("---")
+
+    //
+    println(if (x > y) "x is greater than y" else "x is not greater than y")
 }
