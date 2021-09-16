7a59b554da5fd0ce131e630212e3180e49d1faae
unknown <rodionsharlov@gmail.com>
2021-09-16 23:37:31 +0800

    Add some Chapter 2 functionality

--- PhraseOMatic/src/main/kotlin/Main.kt
+++ PhraseOMatic/src/main/kotlin/Main.kt
@@ -1,6 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.Math.random
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    val wordArray1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
+    val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
+    val wordArray3 = arrayOf("process", "paradigm", "solution", "portal",
+        "vision")
+
+    val arraySize1 = wordArray1.size
+    val arraySize2 = wordArray2.size
+    val arraySize3 = wordArray3.size
+
+    val rand1 = (random() * arraySize1).toInt()
+    val rand2 = (random() * arraySize2).toInt()
+    val rand3 = (random() * arraySize3).toInt()
+
+    val phrase = "${wordArray1[rand1]} ${wordArray2[rand2]} ${wordArray3[rand3]}"
+    println(phrase)
+
+    //
+    var myArray: Array<Byte> = arrayOf(1, 2, 3)
+
 }
