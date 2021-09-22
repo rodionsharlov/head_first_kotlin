d6c6d952124e612f3d662fcca7e4df054ad7d843
unknown <rodionsharlov@gmail.com>
2021-09-22 13:19:46 +0800

    Done with Chapter 5

--- Animals/src/main/kotlin/Main.kt
+++ Animals/src/main/kotlin/Main.kt
@@ -1,3 +1,82 @@
+open class Animal {
+    open val image  = ""
+    open val food = ""
+    open val habitat = ""
+    var hunger = 10
+
+    open fun makeNoise() {
+        println("The animal is making a noise")
+    }
+
+    open fun eat() {
+        println("The animal is eating")
+    }
+
+    open fun roam() {
+        println("The animal is roaming")
+    }
+
+    open fun sleep() {
+        println("The animal is sleeping")
+    }
+}
+
+//
+class Hippo : Animal() {
+    override val image = "hippo.jpg"
+    override val food = "grass"
+    override val habitat = "water"
+
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+
+    override fun eat() {
+        println("The Hippo is eating $food")
+    }
+}
+
+open class Canine : Animal() {
+    override fun roam() {
+        println("The Canine is roaming")
+    }
+}
+
+class Wolf : Canine() {
+    override val image = "wolf.jpg"
+    override val food ="meat"
+    override val habitat = "forests"
+
+    override fun makeNoise() {
+        println("Hoooooowl!")
+    }
+
+    override fun eat() {
+        println("The Wolf is eating $food")
+    }
+}
+
+class Vet {
+    fun giveShot(animal: Animal) {
+        animal.makeNoise()
+    }
+}
+
 fun main(args: Array<String>) {
-    println("This is Sparta!")
+    val animals = arrayOf(Hippo(), Wolf())
+    for (item in animals) {
+        item.roam()
+        item.eat()
+    }
+
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+
+    with(vet) {
+
+        giveShot(wolf)
+        giveShot(hippo)
+    }
+
 }
