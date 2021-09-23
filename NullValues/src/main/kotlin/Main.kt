457e71b6edbbfce955697f9d96a002db20d7dfaf
unknown <rodionsharlov@gmail.com>
2021-09-23 12:26:12 +0800

    Done with Chapter 8 (null, exception)

--- NullValues/src/main/kotlin/Main.kt
+++ NullValues/src/main/kotlin/Main.kt
@@ -1,3 +1,47 @@
+class Wolf {
+    var hunger = 10
+    val food = "meat"
+
+    fun eat() {
+        println("The wolf is eating $food")
+    }
+}
+
+class MyWolf {
+    var wolf: Wolf? = Wolf()
+
+    fun myFunction() {
+        wolf?.eat()
+    }
+}
+
+fun getAlphaWolf() : Wolf? {
+    return Wolf()
+}
+
 fun main(args: Array<String>) {
-    print("Ha!")
+    var w: Wolf? = Wolf()
+
+    if (w != null) w.eat()
+
+    var x = w?.hunger
+    println("The value of x is $x")
+
+    var y = w?.hunger ?: -1
+    println("The value of y is $y")
+
+    var myWolf = MyWolf()
+    myWolf?.wolf?.hunger = 8
+    println("The value if myWolf?.wolf?.hunger is ${myWolf?.wolf?.hunger}")
+
+    var myArray = arrayOf("Hi", "Hello", null)
+    for (item in myArray) {
+        item?.let {println(it)}
+    }
+
+    getAlphaWolf()?.let { it.eat() }
+
+    w = Wolf()
+    w = null
+    var z = w?.hunger
 }
