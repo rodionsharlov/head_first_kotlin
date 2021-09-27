f2d1c433f875a02551049a12662a8c99dc57366f
unknown <rodionsharlov@gmail.com>
2021-09-27 09:40:23 +0800

    Add initial Chapter 12 project. Built-in HOFs

---
+++ Groceries/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+data class Grocery(val name: String, val category: String,
+                   val unit: String, val unitPrice: Double,
+                   val quantity: Int)
+
+fun main(args: Array<String>) {
+    val groceries = listOf(Grocery("Tomatoes","Vegetable","lb",3.0,3),
+        Grocery("Mushrooms","Vegetable","lb",4.0,1),
+        Grocery("Bagels","Bakery","Pack",1.5,2),
+        Grocery("Olive oil","Pantry","Bottle",6.0,3),
+        Grocery("Ice cream","Frozen","Pack",3.0,2))
+}
